5a237ad455aa222ae07a467438d8e1dac81dbeb1
Scott Ferguson <scott.ferguson.github@gmail.com>
2021-07-07 15:16:42 -0600

    Connection.py updated to check line slopes and compare them within a frame

--- study_examples/connection.py
+++ study_examples/connection.py
@@ -8,6 +8,11 @@ if not os.path.isdir(outdir):
 vid = cv2.VideoCapture('pages.mp4')
 framenum = 0
 
+
+#Checks the pixels : Directly below, below to the left, and below to the right
+#to see if they are also white
+#returns the row,col of the first white pixel found or 0,0 if not found
+#TODO - check more pixels?  - return false condition better? - make horizontal option?
 def find_next(edge,rownum,colnum):
     if edge[rownum+1][colnum-1] == 255:
         return rownum+1, colnum - 1
@@ -15,9 +20,46 @@ def find_next(edge,rownum,colnum):
         return rownum+1, colnum
     if edge[rownum+1][colnum+1] == 255:
         return rownum+1, colnum + 1
-    print("end: " , rownum,colnum," from: ",end = " ")
+    #print("end: " , rownum,colnum," from: ",end = " ")
 
-    return 0,0
+    return rownum,colnum #have it return a boolean with it maybe?, then not 0,0 so outputting is better
+
+def find_next_up(edge,rownum,colnum):
+    if edge[rownum-1][colnum-1] == 255:
+        return rownum-1, colnum - 1
+    if edge[rownum-1][colnum] == 255:
+        return rownum-1, colnum
+    if edge[rownum-1][colnum+1] == 255:
+        return rownum-1, colnum + 1
+    #print("end: " , rownum,colnum," from: ",end = " ")
+
+    return rownum,colnum #have it return a boolean with it maybe?, then not 0,0 so outputting is better
+
+#CHECK IF ANY LINES IN THE LIST MATCH ANOTHER LINE - by being too near or by slope
+def check_same_lines(line_list):
+    slope_list = []
+    pair_list = []
+    for line in line_list:
+        print(line)
+        slope = ( (line[3] - line[0]) / (line[4] - line[1])  )
+
+        #might want to check here to see if there is a slope that already matches --
+        i = 0
+        for s in slope_list:
+
+            if ( (slope - s) / slope > -0.2 and (slope - s) / slope < 0.2):
+                pair_list.append( (i, len(slope_list)) )
+            
+            i += 1
+
+        slope_list.append(slope)
+
+    print(pair_list, "PAIR LIST")
+    print(slope_list, " SLOPES")
+    
+    
+    
+    
 
 while (vid.isOpened()):
     ret, frame = vid.read()
@@ -27,28 +69,73 @@ while (vid.isOpened()):
     edge = cv2.Canny(grayframe, 100, 200)
     if framenum == 22:
         #f = open("edge.txt", "w")
-        rownum = 0
         colnum = 0
-        for row in edge:
-            for col in row:
-                if rownum == 50:
+        rows = [50,150,250,350,450,550,650,750,850,950]
+        lineBottoms = []
+        for row in rows: #remove these loops as they are, only loop through certain rows, not every row
+            for col in edge[row]: #probably make row list [100,200,etc..] , then for col in edge[row] 
+
+
+            #above comments partial done
+            
                     #print(col,end = " ")
                     #print(edge[rownum][colnum],end = " ")
-                    if edge[rownum][colnum] == 255:
-                        notbroken = True
-                        length = 0
-                        chRow = rownum
-                        chCol = colnum
-                        while (notbroken):
-                            chRow,chCol = find_next(edge,chRow,chCol)
-                            if chRow == 0:
-                                notbroken = False
-                            length += 1
-                        print(rownum,colnum, "length :  ",length)
+                if edge[row][colnum] == 255:
+                    
+                    notbroken = True
+                    length = 0
+                    chRow = row
+                    chCol = colnum
+                    tmpR = 0
+
+                    chRowUp = row
+                    chColUp = colnum
+                    tmpRUp = 0
+                    #DO NOT NEED LENGTH UP, DIFFERENT LOOPS THAT ARE NOT DEPENDENT ON IT TO STOP
+                    notBrokenUp = True
+
+                    while (notbroken):
+                        tmpR,chCol = find_next(edge,chRow,chCol)
+                        if tmpR == chRow:
+                            notbroken = False
+                        else: 
+                            chRow = tmpR
+                        length += 1
+                    
+                    while (notBrokenUp):
+                        tmpRUp,chColUp = find_next_up(edge,chRowUp,chColUp)
+                        if tmpRUp == chRowUp:
+                            notBrokenUp = False
+                        else: 
+                            chRowUp = tmpRUp
+                        length += 1
+
+                    if (length > 50):
+                        #print(row,colnum, "length :  ",length)
+                        #add the line to the list lineBottoms
+                        notIn = True
+                        for i in lineBottoms:
+                            y = i[1]
+                            if (chCol == y): #probably change to within 5 columns
+                                notIn = False
+
+                        if notIn:
+                            lineBottoms.append( (chRow,chCol,length,chRowUp,chColUp) )
+                        
+                    
+
 
                 colnum += 1
-            rownum += 1
+            
             colnum = 0
+
+
+        for i in lineBottoms:
+            x,y,z = i[0] , i[1], i[2] 
+            print(x,y," length:",z)
+        print(lineBottoms)
+        check_same_lines(lineBottoms)
+            
             #f.write(",".join(map(str,row)) + "\n")
     #cv2.imwrite(outdir+"/pages.%06d.edge.tiff" % framenum, edge)
     #if framenum == 0:
